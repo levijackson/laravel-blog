fe7b92c2e5c9a815dfacb7d3197185002e6095f2
Levi Jackson <aerosox@gmail.com>
2021-06-24 20:42:19 -0400

    update home page blade file

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/blog', 'App\Http\Controllers\PostController@index');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
