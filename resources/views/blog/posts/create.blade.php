0a120865634ac518e918d2122ae780f9b62a896d
Levi Jackson <aerosox@gmail.com>
2021-06-25 14:12:08 -0400

    display the form values if there is validation error

--- resources/views/blog/posts/create.blade.php
+++ resources/views/blog/posts/create.blade.php
@@ -21,24 +21,24 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" name="title" class="form-control" />
-        </div>
-        <div class="form-group">
-            <label for="metaTitle">Meta Title</label>
-            <input type="text" name="metaTitle" class="form-control" />
+            <input type="text" name="title" class="form-control" value="{{ old('title') }}" />
         </div>
         <div class="form-group">
             <label for="body">Body</label>
-            <textarea name="body" class="form-control"></textarea>
+            <textarea name="body" class="form-control">{{ old('body') }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="metaTitle">Meta Title</label>
+            <input type="text" name="metaTitle" class="form-control" value="{{ old('metaTitle') }}" />
         </div>
         <div class="form-group">
             <label for="metaDescription">Meta Description</label>
-            <textarea name="metaDescription" class="form-control"></textarea>
+            <textarea name="metaDescription" class="form-control">{{ old('metaDescription') }}</textarea>
         </div>
         <div class="form-group">
             <label for="urlSlug">URL Slug (optional)</label>
             <small>If not specified one will be created from the title</small>
-            <input type="text" name="urlSlug" class="form-control" />
+            <input type="text" name="urlSlug" class="form-control" value="{{ old('urlSlug') }}" />
         </div>
         <input type="submit" name="publish" class="btn btn-success" value="Publish" />
         <input type="submit" name="draft" class="btn btn-default" value="Save Draft" />
